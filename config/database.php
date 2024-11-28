b8de30709730ec24343d9438873ebb232d7861f3
Soulra01 <raden.raharja@binus.ac.id>
2024-11-28 15:46:29 +0700

    cek

--- config/database.php
+++ config/database.php
@@ -1,8 +1,22 @@
 <?php
-    $config = [
-        'server' => 'localhost',
-        'username' => 'root',
-        'password' => '',
-        'database' => 'SecureStorage',
-    ];
+$config = [
+    'server' => 'ep-raspy-leaf-a1ogsg7r-pooler.ap-southeast-1.aws.neon.tech',
+    'username' => 'default',
+    'password' => 'qo9mMNHRySQ8',
+    'database' => 'verceldb',
+    'port' => '5432',
+    'sslmode' => 'require',
+];
+
+// Membuat string koneksi
+$conn_string = "host={$config['server']} port={$config['port']} dbname={$config['database']} user={$config['username']} password={$config['password']} sslmode={$config['sslmode']}";
+
+// Koneksi ke PostgreSQL
+$dbconn = pg_connect($conn_string);
+
+if (!$dbconn) {
+    echo "Koneksi gagal!";
+} else {
+    echo "Koneksi berhasil!";
+}
 ?>
